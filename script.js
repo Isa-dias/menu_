da48e40748a04f38d22b8af2d04e8eff5eb76abf
Isa-dias <130784201+Isa-dias@users.noreply.github.com>
2024-02-21 12:02:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+<!DOCTYPE hmtl>
+ <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contagem regressiva</title>
+    <link href="style.css" rel="stylesheet">
+ </head>
+ <body>
+     <section class>
+        <buttom id="blablabla">
+
+
+
+</section> 
